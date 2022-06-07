9ee95402593e8bedf134c501b52ccb37bdd8724a
PAZESTRADA <pazestrada67@gmail.com>
2022-06-07 14:22:51 -0300

    login/logout

--- proyecto_final/accounts/urls.py
+++ proyecto_final/accounts/urls.py
@@ -3,14 +3,17 @@ from django.urls import path
 from django.shortcuts import render
 from django.conf.urls import include
 from . import views
+from django.contrib.auth import views as auth_views
 
 
 
 urlpatterns = [
     path('', views.pagina_principal_accounts),
-    path('login/', views.account_login,name="login"),
     path('register/', views.account_register,name="register"),
     path('profile/', views.account_profile,name="profile"),
+    path('login/', auth_views.LoginView.as_view(template_name= "accounts/login.html"), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name="accounts/logout.html"), name="logout"),
+
 ]
 
 
--- proyecto_final/accounts/views.py
+++ proyecto_final/accounts/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 from django.http import HttpResponse
-
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 def pagina_principal_accounts(request):
@@ -10,9 +11,27 @@ def pagina_principal_accounts(request):
 def account_login(request):
     return render(request,"accounts/login.html")
 
+
 def account_register(request):
-    return render(request,"accounts/register.html")
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f"Cuenta creada, bienvenido al blog {username}!")
+            return redirect("login")
+
+    else:
+        form = UserCreationForm( )
+
+    return render(request,"accounts/register.html", {'form': form} )
     
+
+
 def account_profile(request):
     return render(request,"accounts/profile.html")
     
+
+
+
